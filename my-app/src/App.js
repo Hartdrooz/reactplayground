c74a21dbc331d7c454b7248dc733f0e46a793642
Hugo Girard <hugogirard@Hugos-MacBook-Pro.local>
2018-12-02 22:47:03 -0500

    add more style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -44,7 +44,8 @@ class App extends Component {
 
 	render() {
 		const style = {
-			backgroundColor: 'white',
+			backgroundColor: 'green',
+			color: 'white',
 			font: 'inherit',
 			border: '1px solid blue',
 			padding: '8px',
@@ -67,26 +68,26 @@ class App extends Component {
 							/>
 						);
 					})}
-					{/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-					<Person
-						name={this.state.persons[1].name}
-						age={this.state.persons[1].age}
-						click={this.switchNameHandler.bind(this, 'Julie')}
-						changed={this.nameChangedHandler}
-					>
-						My Hobbies: Racing
-					</Person>
-					<Person name={this.state.persons[2].name} age={this.state.persons[2].age} /> */}
 				</div>
 			);
+
+			style.backgroundColor = 'red';
+		}
+
+		const classes = [];
+		if (this.state.persons.length <= 2) {
+			classes.push('red');
+		}
+		if (this.state.persons.length <= 1) {
+			classes.push('bold');
 		}
 
 		return (
 			<div className="App">
 				<h1>Hi, I'm a React App</h1>
-				<p>This is really working!</p>
+				<p className={classes.join(' ')}>This is really working!</p>
 				<button style={style} onClick={this.togglePersonsHandler}>
-					Switch Name
+					Toggle Persons
 				</button>
 				{/* {this.state.showPersons === true ? ( */}
 				{persons}
